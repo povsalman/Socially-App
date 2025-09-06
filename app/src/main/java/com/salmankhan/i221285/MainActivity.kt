0cfe9b90fb95e481778e1f99f6df1048f36e0155
povsalman <salmankhanbhittani2@gmail.com>
2025-09-06 14:43:09 +0500

    Splash Screen Working

--- app/src/main/java/com/salmankhan/i221285/MainActivity.kt
+++ app/src/main/java/com/salmankhan/i221285/MainActivity.kt
@@ -1,20 +1,52 @@
+//package com.salmankhan.i221285
+//
+//import android.os.Bundle
+//import androidx.activity.enableEdgeToEdge
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
+//
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//    }
+//}
+
+
 package com.salmankhan.i221285
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Launch a coroutine to handle the 3-second delay
+        lifecycleScope.launch {
+            // Wait for 3 seconds
+            delay(3000)
+
+            // Navigate to the SignupActivity
+            val intent = Intent(this@MainActivity, SignupActivity::class.java)
+            startActivity(intent)
+
+            // Finish the splash activity so the user can't navigate back
+            finish()
         }
     }
 }
+
+
