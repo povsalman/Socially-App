10b8f005037b4491ba875c34cc926bdac009fc0e
povsalman <salmankhanbhittani2@gmail.com>
2025-09-07 12:26:55 +0500

    Switch Accounts Page Done

--- app/src/main/java/com/salmankhan/i221285/MainActivity.kt
+++ app/src/main/java/com/salmankhan/i221285/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity() {
             delay(3000)
 
             // Navigate to the SignupActivity
-            val intent = Intent(this@MainActivity, SignupActivity::class.java)
+            val intent = Intent(this@MainActivity, SwitchAccountsActivity::class.java)
             startActivity(intent)
 
             // Finish the splash activity so the user can't navigate back
