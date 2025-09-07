3b54704ce3ef9c95a0ddd060401447ed69e40266
povsalman <salmankhanbhittani2@gmail.com>
2025-09-08 02:23:31 +0500

    Personal DM View Done

--- app/src/main/java/com/salmankhan/i221285/MainActivity.kt
+++ app/src/main/java/com/salmankhan/i221285/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity() {
             delay(3000)
 
             // Navigate to the SignupActivity
-            val intent = Intent(this@MainActivity, DmActivity::class.java)
+            val intent = Intent(this@MainActivity, PersonaldmActivity::class.java)
             startActivity(intent)
 
             // Finish the splash activity so the user can't navigate back
