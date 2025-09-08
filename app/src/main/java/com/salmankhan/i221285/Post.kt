187eea07ec82c50adedccf85e830b2f7c775e083
povsalman <salmankhanbhittani2@gmail.com>
2025-09-08 22:16:00 +0500

    Notification (Both) Pages Done

--- app/src/main/java/com/salmankhan/i221285/Post.kt
+++
@@ -1,9 +0,0 @@
-package com.salmankhan.i221285
-
-data class Post(
-    val username: String,
-    val location: String,
-    val profileImage: Int,
-    val postImage: Int,
-    val caption: String
-)
